ce649ffeb45f819dbe52cddb61e4a77fc9c0c328
Beryl <bsnyder@nucleoware.com>
2013-10-19 23:32:49 -0500

    Working on basic site, Models

--- note/models.py
+++ note/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    content = models.TextField()
+    bible_book = models.CharField(max_length=3)
+    bible_chapter = models.CharField(max_length=3)
+    bible_verse = models.CharField(max_length=3)
+    source = models.CharField(max_length=200)
