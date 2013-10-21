2422a984bca8d9a69d72ea5710ca4bc10c2132e6
Beryl <bsnyder@nucleoware.com>
2013-10-20 22:24:56 -0500

    Working on basic site, Models

--- note/models.py
+++ note/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Note(models.Model):
+    date = models.datetime()
     content = models.TextField()
     bible_book = models.CharField(max_length=3)
     bible_chapter = models.IntegerField()
     bible_verse = models.IntegerField()
     source = models.CharField(max_length=200)
+    user_number = models.IntegerField()
+
+
